cfaae047b6e1537608f5510c5646355e4581eb4a
Clement Cariou <wdclem@hotmail.fr>
2023-03-09 14:30:42 +0200

    exercices 1.3 to 1.5

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -1,50 +1,54 @@
 const App = () => {
-  const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pas data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const course = { 
+  name:'Half Stack application development',
+  parts: [
+  {    
+  name:'Fundamentals of React',
+  exercises: 10
+  },
+  {    
+  name:'Using props to pass data',
+  exercises: 7 
+  },
+  {    
+  name:'State of a component',
+  exercises: 14 
+  }
+ ]
+}
 
   return (
     <div>
-      <Header course={course}/>
-      <Content part1={part1} part2={part2} part3={part3} exercises1={exercises1} exercises2={exercises2} exercises3={exercises3}/>
-      <Total total={exercises1 + exercises2 + exercises3}/>
+      <Header course={course.name}/>
+      <Content parts={course.parts}/>
+      <Total parts={course.parts}/>
     </div>
-  )
-}
+  );
+};
 
-const Total= (props) => {
+const Total= ({parts}) => {
   return (
-    <>
-   <p>{props.total}</p> 
-    </>
-  )
-}
+   <p>Number of exercises {parts[0].exercises + parts[1].exercises + parts[2].exercises}</p> 
+  );
+};
 
-const Content = (props) => {
-  console.log(props)
+const Content = ({parts}) => {
   return (
     <div>
-      <Part content={props.part1} exercises={props.exercises1}/>
-      <Part content={props.part2} exercises={props.exercises2}/> 
-      <Part content={props.part3} exercises={props.exercises2}/> 
+      <Part part={parts[0]} />
+      <Part part={parts[1]} />
+      <Part part={parts[2]} />
     </div>
-  )
-}
+  );
+};
 
-const Part = (props) => {
+const Part = ({part}) => {
  return (
-  <>
-    <p>{props.content} {props.exercises}</p>
-  </>
- ) 
-}
+    <p>{part.name} {part.exercises}</p>
+ );
+};
 
 const Header =({course}) => {
-  //console.log(course)
   return (
     <>
    <h1>{course}</h1> 
