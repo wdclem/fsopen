fabedf625bba42bac2cb2dc010f052249c9f80c3
Clement Cariou <wdclem@hotmail.fr>
2023-03-28 10:33:16 +0300

    display array name with .map

---
+++ part2/phonebook/src/App.js
@@ -0,0 +1,34 @@
+import { useState } from 'react'
+
+const App = () => {
+  const [persons, setPersons] = useState([
+    { name: 'Arto Hellas' }
+  ])
+  const [newName, setNewName] = useState('')
+
+  const handleSubmit = (event) => {
+    event.preventDefault()
+    setPersons([...persons, { name: newName }])
+    setNewName('')
+  }
+
+  return (
+    <div>
+      <h2>Phonebook</h2>
+      <form onSubmit={handleSubmit}>
+        <div>
+          name: <input type="text" value={newName} onChange={(e) => setNewName(e.target.value)} />
+        </div>
+        <div>
+          <button type="submit">add</button>
+        </div>
+        <div>debug: {newName}</div>
+      </form>
+      <h2>Numbers</h2>
+      {persons.map((person, index) => (
+        <div key={index}>{person.name}</div>))}
+    </div>
+  )
+}
+
+export default App
