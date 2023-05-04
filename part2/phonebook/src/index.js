d79947140ce70e6c9633214eaa3d4147b1a0c940
Clement Cariou <wdclem@hotmail.fr>
2023-05-04 11:05:37 +0300

    change to npm, add connection to backend

--- part2/phonebook/src/index.js
+++ part2/phonebook/src/index.js
@@ -2,7 +2,7 @@ import ReactDOM from 'react-dom/client'
 import axios from 'axios'
 import App from './App'
 
-axios.get('http://localhost:3001/persons').then(response => {
+axios.get('http://localhost:3001/api/persons').then(response => {
     const persons = response.data
     ReactDOM.createRoot(document.getElementById('root')).render(<App />)
 })
